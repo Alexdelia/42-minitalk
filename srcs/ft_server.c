9e55130d79bbc2c814acb50cbb99820a2eb4df1b
Alexandre Delille <adelille@student.42.fr>
2021-09-15 16:33:21 +0200

    save

--- srcs/ft_server.c
+++ srcs/ft_server.c
@@ -6,13 +6,18 @@
 /*   By: adelille <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/15 16:10:09 by adelille          #+#    #+#             */
-/*   Updated: 2021/09/15 16:19:28 by adelille         ###   ########.fr       */
+/*   Updated: 2021/09/15 16:33:01 by adelille         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <unistd.h>
 #include <signal.h>
 #include "../libft/includes/libft.h"
+
+static void	ft_sigaction(int sig, siginfo_t *siginfo, void *context)
+{
+
+}
 
 int	main(void)
 {
@@ -21,6 +26,11 @@ int	main(void)
 	ft_ps("Server PID: ");
 	ft_pn(getpid());
 	ft_ps("\n");
+	s_sig.sa_sigaction = ft_sigaction;
 	s_sig.sa_flags = SA_SIGINFO;
+	sigaction(SIGUSR1, &s_sig, 0);
+	sigaction(SIGUSR2, &s_sig, 0);
+	while (1);
+		pause();
 	return (0);
 }
