89e3fa22c444877ea0eb86f02f768dfac7ad52f5
Alexdelia <alexandre.delille.57@gmail.com>
2021-09-16 16:57:13 +0200

    debugging

--- srcs/ft_client.c
+++ srcs/ft_client.c
@@ -6,7 +6,7 @@
 /*   By: adelille <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/15 16:13:23 by adelille          #+#    #+#             */
-/*   Updated: 2021/09/15 16:20:12 by adelille         ###   ########.fr       */
+/*   Updated: 2021/09/16 16:53:49 by adelille         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,11 +14,62 @@
 #include <signal.h>
 #include "../libft/includes/libft.h"
 
+static void	ft_sigaction(int sig)
+{
+	static int	received = 0;
+
+	if (sig == SIGUSR1)
+		++received;
+	else
+	{
+		ft_pn(received);
+		ft_ps("\n");
+		exit(0);
+	}
+}
+
+static void	ft_kill(int pid, char *str)
+{
+	int		i;
+	char	c;
+
+	while (*str)
+	{
+		i = 8;
+		c = *str++;
+		while (i--)
+		{
+			if (c >> i & 1)
+				kill(pid, SIGUSR2);
+			else
+				kill(pid, SIGUSR1);
+			usleep(100);
+		}
+	}
+	i = 8;
+	while (i--)
+	{
+		kill(pid, SIGUSR1);
+		usleep(100);
+	}
+}
+
 int	main(int ac, char **av)
 {
-	ft_pn(ac);
+	struct sigaction	s_sig;
+
+	if (ac != 3 || !ft_strlen(av[2]))
+		return (1);
+	ft_ps("Sent\t: ");
+	ft_pn(ft_strlen(av[2]));
 	ft_ps("\n");
-	ft_ps(av[0]);
-	ft_ps("\n");
+	ft_ps("Received: ");
+	s_sig.sa_handler = ft_sigaction;
+	s_sig.sa_flags = SA_SIGINFO;
+	sigaction(SIGUSR1, &s_sig, 0);
+	sigaction(SIGUSR2, &s_sig, 0);
+	ft_kill(ft_atoi(av[1]), av[2]);
+	while (1)
+		pause();
 	return (0);
 }
--- srcs/ft_server.c
+++ srcs/ft_server.c
@@ -6,7 +6,7 @@
 /*   By: adelille <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/15 16:10:09 by adelille          #+#    #+#             */
-/*   Updated: 2021/09/15 16:33:01 by adelille         ###   ########.fr       */
+/*   Updated: 2021/09/16 16:42:49 by adelille         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,29 @@
 
 static void	ft_sigaction(int sig, siginfo_t *siginfo, void *context)
 {
+	static int				i = 0;
+	static pid_t			client_pid = 0;
+	static unsigned char	c = 0;
 
+	(void)context;
+	if (!client_pid)
+		client_pid = siginfo->si_pid;
+	c |= (sig == SIGUSR2); // bitwise inclusive OR assignement
+	if (++i == 8)
+	{
+		i = 0;
+		if (!c)
+		{
+			kill(client_pid, SIGUSR2);
+			client_pid = 0;
+			return ;
+		}
+		write(1, &c, 1);
+		c = 0;
+		kill(client_pid, SIGUSR1);
+	}
+	else
+		c <<= 1; // bitwise left shift assignement
 }
 
 int	main(void)
@@ -28,9 +50,9 @@ int	main(void)
 	ft_ps("\n");
 	s_sig.sa_sigaction = ft_sigaction;
 	s_sig.sa_flags = SA_SIGINFO;
-	sigaction(SIGUSR1, &s_sig, 0);
+	sigaction(SIGUSR1, &s_sig, 0); // man signal: https://man7.org/linux/man-pages/man7/signal.7.html
 	sigaction(SIGUSR2, &s_sig, 0);
-	while (1);
+	while (1)
 		pause();
 	return (0);
 }
