937408789af73a8555a5900e48e037e9243c4ddd
Alexdelia <alexandre.delille.57@gmail.com>
2021-09-16 20:57:43 +0200

    too much char are written, will debug later

--- srcs/ft_check_arg_client.c
+++ srcs/ft_check_arg_client.c
@@ -6,7 +6,7 @@
 /*   By: adelille <adelille@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/16 18:05:07 by adelille          #+#    #+#             */
-/*   Updated: 2021/09/16 20:51:45 by adelille         ###   ########.fr       */
+/*   Updated: 2021/09/16 20:57:12 by adelille         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -33,6 +33,8 @@ int	ft_check_arg_client(int ac, char **av)
 		return ((ft_pserc("Error: Too few arguments\n", RED)
 				+ ft_pserc("Need PID of server then the message\n", RED))
 			* 0 + 1);
+	if (ac > 3)
+		return (ft_pserc("Error: Too many arguments\n", RED) * 0 + 1);
 	pid = ft_atol(av[1]);
 	if (ft_check_digits(av[1]) == FALSE || pid > INT_MAX || pid < 0)
 		return (ft_pserc("Error: Server PID invalid\n", RED) * 0 + 1);
